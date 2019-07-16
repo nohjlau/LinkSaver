3b4105a278b9b13254a3f2d2213fd79b3d8d0e05
John Lau <nohjlau@gmail.com>
2019-07-16 15:17:01 -0700

    Implemented Responsive design.

--- app.js
+++ app.js
@@ -111,22 +111,24 @@ function displayLinks() {
     let index = 0;
     for(let link of links) {
         let linkHTMLString = `
-        <div class="link panel">
-            <div class="link-options">
-                <button class="btn-sm" onclick="deleteLink(${index})">Delete</button>
-                <button class="btn-sm" onclick="editLink(${index})">Edit</button>
-            </div>
-            <a href="${link.url}"><h1 class="header">${link.title}</h1></a>
-            <p class="link-date">${Date.now()}</p>
-            <div class="categories">
-                Categories:
+        <div class="flex-item">
+            <div class="link panel">
+                <div class="link-options">
+                    <button class="btn-sm" onclick="deleteLink(${index})">Delete</button>
+                    <button class="btn-sm" onclick="editLink(${index})">Edit</button>
+                </div>
+                <a href="${link.url}"><h1 class="header">${link.title}</h1></a>
+                <p class="link-date">${Date.now()}</p>
+                <div class="categories">
+                    Categories:
                 `;
             for(let category of link.categories) {
                 linkHTMLString += `<span class="category">${category}</span>`;
             }
             linkHTMLString += `
                 </div>
-            </div>`;
+            </div>
+        </div>`;
 
     linksList.innerHTML += linkHTMLString;
     index++;
